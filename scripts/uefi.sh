82de2e29ea52fdb4da1bbf0fc7b6cb4af62199ca
ptylenda <tylenda.piotr@gmail.com>
2018-07-26 13:30:16 +0200

    UEFI fix

--- scripts/uefi.sh
+++ scripts/uefi.sh
@@ -3,3 +3,5 @@
 mkdir -p /boot/efi/EFI/BOOT
 cp /boot/efi/EFI/ubuntu/* /boot/efi/EFI/BOOT
 mv /boot/efi/EFI/BOOT/shimx64.efi /boot/efi/EFI/BOOT/BOOTX64.EFI
+
+grub-mkconfig -o /boot/efi/EFI/ubuntu/grub.cfg
